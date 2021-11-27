9f34dddcf63f45037661e240a20b075c83e26dc1
atulkumarrai <iamatul1214@gmail.com>
2021-11-28 00:55:32 +0530

    First commit, configured the operations file

---
+++ Operations.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, max_error
+
+class categorical_handler:
+    def __init__(self,dataframe):
+        self.dataframe=dataframe
+
+    def return_categorical_features(self):
+        df=self.dataframe
+        cat=(df.dtypes=='object')
+        categorical_features=list(cat[cat].index)
+        print("categorical features={}".format(categorical_features))
+        return categorical_features
+
+    def remove_Categorical(self,cat_features):
+        df=self.dataframe
+        updated_df=df.drop(columns=cat_features)
+        return updated_df
+
+    def ordinal_Encoding(self,cat_features):
+        cat_features_ordinal={}
+        df=self.dataframe
+
+        for i in range(len(cat_features)):
+            uniques=df[cat_features[i]].unique()
+            values=range(len(uniques))
+            keys=uniques
+            ordinal_dict=zip(keys,values)
+            cat_features_ordinal.append(ordinal_dict)
+
+        for i in range(len(cat_features_ordinal)):
+            df[cat_features[i]]=df[cat_features[i]].replace(cat_features_ordinal[i])
+        updated_df=pd.DataFrame(df)
+        return updated_df
+
+    def one_Hot_Encoding(self,cat_features):
+        df=self.dataframe
+        one_hot_encode=pd.get_dummies(df[cat_features], drop_first=True)
+        df=df.drop(columns=cat_features)
+        frames=[df,one_hot_encode]
+        updated_df=pd.concat(frames,axis=1)
+        return updated_df
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from Operations import categorical_handler
+import pandas as pd
+
+dataframe=pd.read_csv('melboun_housing_data.csv')
+print(dataframe)
