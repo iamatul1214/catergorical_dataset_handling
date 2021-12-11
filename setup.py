38adc41a811cdd27cee6e3d3e102d8e5f3e9314c
atulkumarrai <iamatul1214@gmail.com>
2021-12-12 00:52:23 +0530

    Updating another version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as file:
 
 setup(
    name='CatDealer',
-   version='1.0.1',
+   version='1.0.2',
    description='A library which can deal with Categorical values in a dataframe',
    license="MIT",
    long_description=long_description,
